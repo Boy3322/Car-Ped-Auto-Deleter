64b80e12e6c9e456d96cb07b1838796f228154ff
Eric <boygamer2007@gmail.com>
2024-10-07 10:32:28 -0400

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,17 @@
+lua54 'yes'
+fx_version 'cerulean'
+
+game 'gta5'
+this_is_a_map 'yes'
+
+author 'Boy Developments'
+description 'No Peds'
+version '1.0.0'
+
+
+client_scripts { 
+    'client.lua',
+}
+    
+    
+dependency '/assetpacks'
